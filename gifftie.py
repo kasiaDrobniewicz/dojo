c3275803049cf82fa312fd31eb8775b04d17ce5a
Kasia Drobniewicz <kasia.drobniewicz@gmail.com>
2017-08-23 10:28:24 +0200

    dfsdsdf

--- gifftie.py
+++ gifftie.py
@@ -1,9 +1,10 @@
 def introduce():
-    print('Hello I\' Gittbfdgfvbddvxie')
+    print('Hello I\' bbb')
 introduce()
 
 
 def add(a, b):
     c = a+b
+    c = c+1
     print(c)
-add(1, 2)    
+add(1, 6)    
